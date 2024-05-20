5da00325ed412bd634d96d100240faf6bfe55dc8
abd-elqader <abdelqader.montaser@gmail.com>
2024-05-20 14:28:39 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+
+
+var bookmark = document.getElementById("bookmark");
+var url = document.getElementById('url');
+
+function addBookmark(){
+    
+    console.log(
+        bookmark.value,
+        
+        url.value
+    );
+    
+}
