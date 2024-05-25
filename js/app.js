26561aaa114cc528aeef542399ada3fc368c4538
shimamer <eng.shima.998@gmail.com>
2024-05-25 19:46:58 +0300

    update and search feature

--- js/app.js
+++ js/app.js
@@ -6,30 +6,35 @@ if(localStorage.getItem('data') !== null){
 }
 var bookmark = document.getElementById("bookmark");
 var url = document.getElementById('url');
+var search = document.querySelector('label input');
 
 function addBookmark(){
 
-    if (!getBookmarkValidation(bookmark.value)) {
-        alert('wrong bookmark')
-    }
-    if(!getUrlValidation(url.value)){
-        alert('wrong url')
-    }
+    // if (!getBookmarkValidation(bookmark.value)) {
+    //     alert('wrong bookmark')
+    // }
+    // if(!getUrlValidation(url.value)){
+    //     alert('wrong url')
+    // }
 
-    if(!getUrlValidation(url.value) || !getBookmarkValidation(bookmark.value)){
-        return 0;
-    }
-    var record = {
-        "bookmark": bookmark.value,
-        "url": url.value,
-    }
+    // if(!getUrlValidation(url.value) || !getBookmarkValidation(bookmark.value)){
+    //     return 0;
+    // }
 
-    data.push(record);
-    var strObj = JSON.stringify(data);
-    localStorage.setItem('data', strObj);
+    if(validation()){
 
-    showData();
-    clearFields();
+        var record = {
+            "bookmark": bookmark.value,
+            "url": url.value,
+        }
+
+        data.push(record);
+        var strObj = JSON.stringify(data);
+        localStorage.setItem('data', strObj);
+
+        showData();
+        clearFields();
+    }
 }
 
 function clearFields () {
@@ -48,38 +53,123 @@ function showData() {
                             <a href="${data[i].url}" target="_blank" class="btn btn-success"><i class="fa fa-solid fa-eye"></i> Visit</a>
                         </td>
                         <td>
-                            <a onclick="deleteItem(${i})" class="btn btn-danger"><i class="fa fa-solid fa-trash"> Delete</i></a>
+                            <a onclick="editBookmark(${i})" class="btn btn-warning text-white"><i class="fa fa-solid fa-edit"></i> Edit</a>
+                        </td>
+                        <td>
+                            <a onclick="deleteItem(${i})" class="btn btn-danger"><i class="fa fa-solid fa-trash"></i> Delete</a>
                         </td>
                     </tr>
         `
     }
     document.getElementById('body').innerHTML = html;
-    console.log(data);
+}
+
+function validation () {
+    if (!getBookmarkValidation(bookmark.value)) {
+        alert('wrong bookmark')
+        return false;
+    }
+    if(!getUrlValidation(url.value)){
+        alert('wrong url')
+        return false;
+    }
+    if(!getUrlValidation(url.value) || !getBookmarkValidation(bookmark.value)){
+        return false;
+    }
+    return true
 }
 
 function getUrlValidation(input) {
     var regex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
 
     if (regex.test(input)) {
-        return 1;
+        return true;
     } else {
-        return 0;
+        return false;
     }
 }
 
 function getBookmarkValidation(input) {
-    var regex = /^[a-zA-Z0-9]{3,}$/;
+    var regex = /^[a-zA-Z0-9 ]{3,}$/;
 
     if (regex.test(input)) {
-        return 1;
+        return true;
     } else {
-        return 0;
+        return false;
     }
 }
 
 function deleteItem (itemId) {
-    console.log(typeof(data));
     data.splice(itemId,1);
     showData();
     localStorage.setItem('data',JSON.stringify(data));
+}
+
+
+function tableSearch (term) {
+    
+    if(localStorage.getItem('data') !== null){
+        data = JSON.parse(localStorage.getItem('data'));
+        showData();
+    }
+
+    var html='';
+
+    for (let i = 0; i < data.length; i++) {
+
+        console.log(data[i].bookmark.trim().toLowerCase().includes(term.toLowerCase()));
+        if(data[i].bookmark.trim().toLowerCase().includes(term.toLowerCase())){
+            console.log(data[i].bookmark);   
+            html += `
+                <tr>
+                    <td>${i+1}</td>
+                    <td>${data[i].bookmark}</td>
+                    <td>
+                        <a href="${data[i].url}" target="_blank" class="btn btn-success"><i class="fa fa-solid fa-eye"></i> Visit</a>
+                    </td>
+                    <td>
+                        <a onclick="editBookmark(${i})" class="btn btn-warning"><i class="fa fa-solid fa-edit"></i> Edit</a>
+                    </td>
+                    <td>
+                        <a onclick="deleteItem(${i})" class="btn btn-danger"><i class="fa fa-solid fa-trash"></i> Delete</a>
+                    </td>
+                </tr>
+            `
+        }
+        document.getElementById('body').innerHTML = html;
+    }
+    // 
+}
+
+
+search.addEventListener('input',function(e) {
+    tableSearch(e.target.value);
+})
+
+function editBookmark (item) {
+
+    bookmark.value = data[item].bookmark;
+    url.value = data[item].url;
+    document.getElementById('actionButton').innerHTML = `<button onclick="updateBookmark(${item})" class="btn btn-warning px-5 mb-4">update</button>`;
+    console.log(data[item]);
+    
+
+
+    console.log(item);
+}
+
+
+function updateBookmark(i) {
+    data[i].bookmark = bookmark.value;
+    data[i].url = url.value;    
+
+    validation();
+
+    localStorage.setItem('data' , JSON.stringify(data))
+
+    document.getElementById('actionButton').innerHTML = `<button onclick="addBookmark()" class="btn btn-danger px-5 mb-4">Submit</button>`;
+
+    showData();
+    clearFields();
+    
 }
